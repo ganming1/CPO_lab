435dfaa4a1134152693ec924d9707a3dce4f2ac8
ganming1 <13635845781@qq.com>
2020-05-06 23:04:19 +0800

    mutable

--- lab1.py
+++ lab1.py
@@ -64,33 +64,102 @@ class SignLinklist:
 
 # 哈希表 类似于集合
 class HashTable:
-
-    def __init__(self, size=10):
+    def __init__(self, size = 10, list = None):
         self.size = size
         self.T = [SignLinklist() for x in range(self.size)]
+        if (list):
+            self.list_to_hashTable(list)
 
-    def h(self, k):
+    def size(self):
+        return self.size
+
+    def itm_size(self):
+        s = 0
+        for i in range(self.size):
+            for j in self.T[i]:
+                s += 1
+        return s
+
+    def hash(self, k):
         return k % self.size
 
+    def hashTable_to_list(self):
+        lst = []
+        for i in range(self.size):
+            for j in self.T[i]:
+                lst.append(j)
+        return lst
+
+    def list_to_hashTable(self, lst):
+        for i in range(len(lst)):
+            self.insert(lst[i])
+
     def insert(self, k):
-        i = self.h(k)
+        i = self.hash(k)
         if self.find(k):
-            print('Duplicated Insert')
+            # print('Duplicated Insert')
+            return self
         else:
             self.T[i].append(k)
+            return self
+
+    def delete(self, k):
+        i = k % self.size
+        for j in self.T[i]:
+            if j == k and j == self.T[i].head.item:
+                self.T[i].head = self.T[i].head.next
+                break
+            if j == k and j == self.T[i].tail.item:
+               pre = self.T[i].head
+               while pre.next != self.T[i].tail:
+                   pre = pre.next
+               self.T[i].tail = None
+               pre.next = None
+               print('\n'.join(map(str, ht.T)))
+               self.T[i].tail = pre
+               break
+            else:
+                pre = self.T[i].head
+                print(type(pre.next))
+                while pre.next.item != k:
+                    pre = pre.next
+                if pre.next.item == k:
+                    pre.next = pre.next.next
+                    break
+        return self
+
 
     def find(self, k):
-        i = self.h(k)
+        i = self.hash(k)
         return self.T[i].find(k)
-
 
 if __name__ == '__main__':
     ht = HashTable()
     ht.insert(1)
+    ht.insert(11)
+    ht.insert(21)
     ht.insert(18)
     ht.insert(29)
-    ht.insert(31)
+    ht.insert(28)
+    ht.insert(38)
+    ht.insert(219)
+
+#     print('\n'.join(map(str, ht.T)))
+#
+#     print(ht.find(210))
+    print('\n'.join(map(str, ht.T)))
+    # ht.delete(1)
+    ht.delete(28)
     print('\n'.join(map(str, ht.T)))
 
-    print(ht.find(210))
+
+    # ls = ht.hashTable_to_list()
+    #
+    # print(ls)
+    # print(ht.itm_size())
+    #
+    # ht2 = HashTable()
+    # ht2.list_to_hashTable([1,2,4,5,10,20,45])
+    # ls2 = ht2.hashTable_to_list()
+    # print(ls2)
 
